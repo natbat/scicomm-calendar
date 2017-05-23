3e93825cc672e4737771f606a4bc273ecc527f14
Natalie Downe <nat@natbat.net>
2017-05-22 18:05:31 -0700

    Renamed some tests
    Also added a test for if the screen name does not match, and a test
    for case insensitive screen names.

--- tests.py
+++ tests.py
@@ -6,32 +6,42 @@ class TestTweetMatchesRules(unittest.TestCase):
     # All hashtags by the time they get to the tweet_matches_rules have been 
     # already lowercased, so no need to test that
 
-    def test_basicRetweet(self):
+    def test_rule(self):
         screen_name = "natbat"
         full_text = "I like #hedgehogs"
         rules = {"natbat": ["hedgehogs"]}
         self.assertTrue(tweet_matches_rules(screen_name, full_text, rules))
 
-    def test_basicRetweetCaps(self):
+    def test_rule_case_insensitive_hashtag(self):
         screen_name = "natbat"
         full_text = "I like #HedgehogOrNot"
         rules = {"natbat": ["hedgehogornot"]}
         self.assertTrue(tweet_matches_rules(screen_name, full_text, rules))
 
-    def test_basicNoRetweet(self):
+    def test_rule_case_insensitive_screen_name(self):
+        screen_name = "NatBat"
+        full_text = "I like #HedgehogOrNot"
+        rules = {"natbat": ["hedgehogornot"]}
+        self.assertTrue(tweet_matches_rules(screen_name, full_text, rules))
+
+    def test_rule_no_matching_hashtag(self):
         screen_name = "natbat"
         full_text = "I like Racoons"
         rules = {"natbat": ["hedgehogornot"]}
         self.assertFalse(tweet_matches_rules(screen_name, full_text, rules))
 
-    def test_noHashNoRetweet(self):
+    def test_rule_no_matching_screen_name(self):
+        screen_name = "frog"
+        full_text = "I like #hedgehogornot"
+        rules = {"natbat": ["hedgehogornot"]}
+        self.assertFalse(tweet_matches_rules(screen_name, full_text, rules))
+
+    def test_rule_no_hash_on_hashtag(self):
         screen_name = "natbat"
         full_text = "I like HedgehogOrNot"
         rules = {"natbat": ["hedgehogornot"]}
         self.assertFalse(tweet_matches_rules(screen_name, full_text, rules))
 
-
-    # TODO break if the config file is valid
 
 if __name__ == '__main__':
     unittest.main()
